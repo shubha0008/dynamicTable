f41ca66fa50c631f6c0d2ddbb60d2da23d625a53
shubha0008 <shubharashmi101@gmail.com>
2020-12-30 22:01:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+import  "./styles/style.scss";
+import TaskList from "./Components/Tasklist"
+    
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class addNewColumn extends React.Component{
+//   //this.setState({ [e.target.name]: e.target.value });
+// render(){
+//   return(
+//     <select className="form-control" >
+//             <option value="Status">Status</option>
+//             <option value="Text">Text</option>
+//             <option value="People">People</option>
+//             <option value="Timeline">Timeline</option>
+//             <option value="Date">Date</option>
+//             <option value="Tags">Tags</option>
+//             <option value="Numbers">Numbers</option>
+//           </select>
+//   )
+// }
+  
+// }
+ 
+class App extends React.Component {
+ 
+  state = {
+    taskList: [{ index: Math.random(), ThingsTodo: "", Owner: "", Status: "", DueDate: "" ,Priority:"",MyStatus:"" }],
+  };
+  
+  handleChange = (e) => {
+    if (["projectName", "task", "taskNotes", "taskStatus"].includes(e.target.name)) {
+        let taskList = [...this.state.taskList]
+        taskList[e.target.dataset.id][e.target.name] = e.target.value;
+    } else {
+        this.setState({ [e.target.name]: e.target.value })
+    }
 }
+
+  addtaskNewRow = (e) => {
+    this.setState((prevState) => ({
+      taskList: [...prevState.taskList, { index: Math.random(), ThingsTodo: "", Owner: "", Status: "", DueDate: "" ,Priority:"",MyStatus:""}],
+  }));
+
+  }
+  
+  addNewColumn = () => {
+
+    var container = document.createElement("div");
+    var select = document.createElement("select");
+
+    var option1 = document.createElement("option");
+    option1.setAttribute("value","Status");
+    var option2 = document.createElement("option");
+    option2.setAttribute("value","Text");
+    var option3 = document.createElement("option");
+    option3.setAttribute("value","People")
+    var option4 = document.createElement("option");
+    option4.setAttribute("value","Timeline")
+    var option5 = document.createElement("option");
+    option5.setAttribute("value","Date")
+    var option6 = document.createElement("option");
+    option6.setAttribute("value","Tags")
+    var option7 = document.createElement("option");
+    option7.setAttribute("value","Numbers")
+
+    select.appendChild(option1);
+    select.appendChild(option2);
+    select.appendChild(option3);
+    select.appendChild(option4);
+    select.appendChild(option5);
+    select.appendChild(option6);
+    select.appendChild(option7);
+
+    container.appendChild(select);
+
+    document.getElementById("columnOptions").onfocus = document.getElementsByTagName('body')[0].appendChild(container); 
+  }
+  
+
+clickOnDelete(record) {
+    this.setState({
+        taskList: this.state.taskList.filter(r => r !== record)
+    });
+}
+
+
+ 
+  render() {
+    let { taskList} = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
+      <Draggable
+        
+        axis="x"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+        <div>
+         <table className="tableborder">
+         <thead>
+         <tr>
+           <th style={{width:"1%"}}></th>
+           <th>Things to do</th>
+           <th className="handle">Owner</th>
+           <th className="handle">Status</th>
+           <th className="handle">Due date</th>
+           <th className="handle">Priority</th>
+           <th className="handle">MyStatus</th>
+           <th className="handle" > <button onClick={this.addNewColumn} id = "columnOptions">  + </button></th>
+         </tr>
+         </thead>
+         <tbody>
+         <TaskList add={this.addtaskNewRow}  delete={this.clickOnDelete.bind(this)} taskList={taskList} />
+         </tbody>
+         <tfoot>
+         <button onClick={this.addtaskNewRow} type="button" className="btn btn-primary text-center">+</button>
+         </tfoot>
+         </table>
+         
+        </div>
+      </Draggable>
+      </form>
+      </div>
+    );
+  }
+}
+ 
+// ReactDOM.render(<App/>, document.body);
+//   );
+
 
 export default App;
---
+++ src/Components/Tasklist.js
@@ -0,0 +1,105 @@
+import React from "react"
+import  "../styles/style.css";
+
+
+const TaskList = (props) => {
+    
+    // const handleFocus = () =>
+    // {
+    //     let n = 0;
+    //     document.getElementsByTagName("datalist")[n].style.display = 'flex';
+        
+       
+    //         for (var option of document.getElementsByTagName("datalist")[n].options) {
+    //             option.onclick = function () {
+    //                 document.getElementById("input").value = this.value;
+    //                 document.getElementById("input").backgroundColor = this.backgroundColor;
+    //             }
+    //             console.log(n)
+    //             n++;
+    //         }
+    //         n++;
+        
+    // }
+     
+    // const removeFocus = () =>{
+    //     document.getElementsByTagName("datalist")[0].style.display = 'none';
+        
+        
+    // }
+    
+
+
+return (
+props.taskList.map((val, idx) => {
+let ThingsTodo = `ThingsTodo-${idx}`, Owner = `Owner-${idx}`, Status = `Status-${idx}`, DueDate = `DueDate-${idx}`,Priority = `Priority-${idx}` ,MyStatus = `MyStatus-${idx}` 
+return (
+<tr key={val.index}>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <td style={{backgroundColor:"skyblue"}}> </td>
+<td>
+<input type="text" name="ThingsTodo" data-id={idx} id={ThingsTodo} className="form-control " />
+</td>
+
+<td>
+<input type="text" name="Owner" id={Owner} data-id={idx} className="form-control " />
+</td>
+
+<td>
+<input
+  list="Status"
+  name="Status"
+  id="input"
+  autocomplete="off"
+  onFocus = { () => {
+    document.getElementsByTagName("datalist")[idx].style.display = 'flex';
+    
+        for (var option of document.getElementsByTagName("datalist")[idx].options) {
+            option.onclick = function () {
+                document.getElementById("input").value = this.value;
+                //document.getElementById("input").backgroundColor = this.backgroundColor;
+            }
+        }
+    }}
+  
+></input>
+<datalist name="Status" id={Status} data-id={idx} className="form-control" onMouseLeave = { () => {document.getElementsByTagName("datalist")[idx].style.display = 'none';}} >
+  <div className="flex-container" style={{display:"flex",flexDirection:"column"}}>
+     <option value="Working on it"  style={{ backgroundColor:"yellow"}}>Working on it</option>
+     <option value="Stuck"  style={{ backgroundColor:"red"}}>Stuck</option>
+     <option value="Critical" style={{ backgroundColor:"purple"}}>Critical</option>
+  </div>
+  <div className="flex-container" style={{display:"flex",flexDirection:"column"}}>
+    <option value="Waiting for review" style={{ backgroundColor:"pink"}}>Waiting for review</option>
+    <option value="Done" style={{ backgroundColor:"green"}}>Done</option>
+  </div>
+  
+</datalist>
+
+
+</td>
+
+<td>
+<input type="date" name="DueDate" id={DueDate} data-id={idx} className="form-control"/>
+</td>
+
+<td>
+ <select name="Priority" id={Priority} data-id={idx} className="form-control" >
+  <option value="Urgent">Urgent</option>
+  <option value="High">High</option>
+  <option value="Medium">Medium</option>
+  <option value="Low">Low</option>
+ </select>
+</td>
+<td>
+<input type="text" name="MyStatus" id={MyStatus} data-id={idx} className="form-control"></input>
+</td>
+<td>
+<button className="btn btn-danger" onClick={(() => props.delete(val))} >-</button>
+</td>
+</tr >
+)
+})
+)
+}
+export default TaskList;
